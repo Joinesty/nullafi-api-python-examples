39fc2f8d4394b7530ffb0fd2eb8d43c06e2bd9cd
eldersantos <elder.santos@gmail.com>
2020-10-16 13:35:18 -0300

    adding vault list

--- main.py
+++ main.py
@@ -56,9 +56,35 @@ def authenticate():
             res_json = json.loads(res_body.decode("utf-8"))
             return res_json['token']
 
+def list_vaults(api_token):
+    req = urllib.request.Request(url=os.environ["NULLAFI_API_URL"] + "/v2/vault", 
+        method='GET', 
+        headers={'Authorization': api_token}
+    )
+
+    with urllib.request.urlopen(req, timeout=5) as f:
+        if (f.status == 200):
+            res_body = f.read()
+            res_json = json.loads(res_body.decode("utf-8"))
+            return res_json
+
     
 if __name__ == "__main__":
+    """
+    Loading environment varibles from .env file
+    Please confirm you have a .env file in the place
+    """
     load_env()
-    api_token = authenticate()
 
+    """
+    Doing API authentication using the API KEY from env file
+    """
+    api_token = authenticate()
     print(f"Your token is: {api_token}")
+
+    """
+    Getting the list of vaults available
+    After sign up on the dashboard you have 1 Communication vaults and 1 Static vault enabled by default.
+    """
+    vault_list = list_vaults(api_token)
+    print(f"List of vaults is: {vault_list}")
